7069ded5eb12701b037d591ee3bee2c3970987fc
Leonard Hecker <lhecker@microsoft.com>
2022-05-07 16:54:31 +0200

    Change clang-format settings

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include <Windows.h>
 #include <conio.h>
 #include <fcntl.h>
-#include <io.h>
 #else
 #include <termios.h>
 #include <unistd.h>
@@ -15,92 +14,71 @@
 
 #include <charconv>
 #include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <string>
 #include <string_view>
 #include <vector>
-#include <cstring>
-#include <cmath>
-#include <cstdio>
 
-#define escape(s) "\x1b" s
-
-static void write_console(const std::string_view& s) noexcept
-{
+static void write_console(const std::string_view& s) noexcept {
 #ifdef _WIN32
     WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), s.data(), s.size(), nullptr, nullptr);
 #else
-	write(STDOUT_FILENO, s.data(), s.size());
+    write(STDOUT_FILENO, s.data(), s.size());
 #endif
 }
 
-static void cleanup()
-{
-	// Start with a fresh line, show cursor again, disable Synchronized Output.
-	write_console("\n\033[?25h\033[?2026l");
+static void cleanup() {
+    // Start with a fresh line, show cursor again, disable Synchronized Output.
+    write_console("\n\033[?25h\033[?2026l");
 }
 
-static std::string read_next_vt() noexcept
-{
+static std::string read_next_vt() noexcept {
     std::string buffer;
     size_t state = 0;
 
-    for (;;)
-    {
+    for (;;) {
 #ifdef _WIN32
         const auto ch = _getch_nolock();
 #else
         const auto ch = getchar();
-        if (ch == EOF)
-        {
+        if (ch == EOF) {
             return "";
         }
 #endif
         buffer.push_back(static_cast<char>(ch));
 
-        switch (state)
-        {
+        switch (state) {
         case 0: // Fe Escape sequence
-            if (ch == 0x1B) // ESC
-            {
-                state++;
+            if (ch == 0x1B) {
+                state++; // ESC
             }
             break;
         case 1: // Control Sequence Introducer
-            if (ch == 0x5B) // [
-            {
-                state++;
-            }
-            else
-            {
+            if (ch == 0x5B) {
+                state++; // [
+            } else {
                 buffer.clear();
                 state = 0;
             }
             break;
         case 2: // parameter bytes in the range 0x30–0x3F
-            if (ch >= 0x30 && ch <= 0x3F)
-            {
+            if (ch >= 0x30 && ch <= 0x3F) {
                 break;
-            }
-            else
-            {
+            } else {
                 state++;
             }
         case 3: // intermediate bytes in the range 0x20–0x2F
-            if (ch >= 0x20 && ch <= 0x2F)
-            {
+            if (ch >= 0x20 && ch <= 0x2F) {
                 break;
-            }
-            else
-            {
+            } else {
                 state++;
             }
         case 4: // final byte in the range 0x40–0x7E
-            if (ch >= 0x40 && ch <= 0x7E)
-            {
+            if (ch >= 0x40 && ch <= 0x7E) {
                 return buffer;
-            }
-            else
-            {
+            } else {
                 buffer.clear();
                 state = 0;
             }
@@ -110,18 +88,15 @@ static std::string read_next_vt() noexcept
     }
 }
 
-static void get_window_size(size_t& dx, size_t& dy)
-{
+static void get_window_size(size_t& dx, size_t& dy) {
     write_console(
         "\x1b[9999;9999H" // Cursor Position (CUP)
-        "\x1b[6n" // Report Cursor Position (CPR) using Device Status Report (DSR) 6
+        "\x1b[6n"         // Report Cursor Position (CPR) using Device Status Report (DSR) 6
     );
 
-    for (;;)
-    {
+    for (;;) {
         const auto seq = read_next_vt();
-        if (seq.size() <= 3 || seq.back() != 'R')
-        {
+        if (seq.size() <= 3 || seq.back() != 'R') {
             continue;
         }
 
@@ -129,14 +104,12 @@ static void get_window_size(size_t& dx, size_t& dy)
         const auto beg = seq.data() + 2;
         const auto end = beg + count;
         const auto mid = std::find(beg, end, ';');
-        if (!mid)
-        {
+        if (!mid) {
             abort();
         }
 
         static constexpr auto parse = [](const char* beg, const char* end, size_t& out) {
-            if (std::from_chars(beg, end, out, 10).ec != std::errc())
-            {
+            if (std::from_chars(beg, end, out, 10).ec != std::errc()) {
                 abort();
             }
         };
@@ -149,15 +122,13 @@ static void get_window_size(size_t& dx, size_t& dy)
     }
 }
 
-static void hue_to_rgb(double color_index, double num_colors, uint8_t& r, uint8_t& g, uint8_t& b)
-{
+static void hue_to_rgb(double color_index, double num_colors, uint8_t& r, uint8_t& g, uint8_t& b) {
     // https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
     const auto h = color_index / num_colors * 360.0;
     const auto hh = static_cast<int>(h / 60.0);
     const auto v = static_cast<int>(256.0 / 60.0 * std::fmod(h, 60.0));
 
-    switch (hh % 6)
-    {
+    switch (hh % 6) {
     case 0:
         r = 255;
         g = v;
@@ -191,17 +162,18 @@ static void hue_to_rgb(double color_index, double num_colors, uint8_t& r, uint8_
     }
 }
 
-static void signalHandler(int)
-{
-	cleanup();
-	exit(EXIT_SUCCESS);
+static void signalHandler(int) {
+    cleanup();
+    exit(EXIT_SUCCESS);
 }
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
 #ifdef _WIN32
     SetConsoleCP(CP_UTF8);
-    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), ENABLE_PROCESSED_OUTPUT | ENABLE_WRAP_AT_EOL_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN);
+    SetConsoleMode(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        ENABLE_PROCESSED_OUTPUT | ENABLE_WRAP_AT_EOL_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN
+    );
 #else
     // Disable line buffering and automatic echo for stdin.
     // This allows us to call getchar() without blocking until the next newline.
@@ -211,11 +183,10 @@ int main(int argc, const char* argv[])
     tcsetattr(0, TCSANOW, &term);
     // Disable stdout buffering, allowing us to drop the fflush().
     setvbuf(stdout, nullptr, _IONBF, 0);
-	signal(SIGINT, signalHandler);
+    signal(SIGINT, signalHandler);
 #endif
 
-    if (argc != 1 && argc != 2)
-    {
+    if (argc != 1 && argc != 2) {
         fprintf(stderr, "usage: rainbowbench <num_colors>\n");
         return 1;
     }
@@ -223,10 +194,8 @@ int main(int argc, const char* argv[])
     // HSV offers at most 1530 distinct colors in 8-bit RGB
     static constexpr size_t total_rainbow_colors = 1530;
     size_t num_colors = total_rainbow_colors;
-    if (argc == 2)
-    {
-        if (std::from_chars(argv[1], argv[1] + strlen(argv[1]), num_colors).ec != std::errc())
-        {
+    if (argc == 2) {
+        if (std::from_chars(argv[1], argv[1] + strlen(argv[1]), num_colors).ec != std::errc()) {
             fprintf(stderr, "invalid num_colors\n");
             return 1;
         }
@@ -251,15 +220,25 @@ int main(int argc, const char* argv[])
         uint8_t bp;
         hue_to_rgb(num_colors - 1, num_colors, rp, gp, bp);
 
-        for (size_t i = 0, count = num_colors + dx; i < count; ++i)
-        {
+        for (size_t i = 0, count = num_colors + dx; i < count; ++i) {
             hue_to_rgb(i, num_colors, r, g, b);
 
             // Using ▀ would be graphically more pleasing, but in this benchmark we want to
             // test rendering performance and DirectWrite, as used in Windows Terminal,
             // has a very poor font-fallback performance. If we were to use ▀, we'd
             // primarily test how DirectWrite's instead of Terminal's performance.
-            const auto length = snprintf(buffer, std::ssize(buffer), "\x1b[38;2;%d;%d;%d;48;2;%d;%d;%dm%c", rp, gp, bp, r, g, b, static_cast<char>('A' + i % ('Z' - 'A' + 1)));
+            const auto length = snprintf(
+                buffer,
+                std::ssize(buffer),
+                "\x1b[38;2;%d;%d;%d;48;2;%d;%d;%dm%c",
+                rp,
+                gp,
+                bp,
+                r,
+                g,
+                b,
+                static_cast<char>('A' + i % ('Z' - 'A' + 1))
+            );
             rainbow_indices.push_back(rainbow.size());
             rainbow.append(buffer, length);
 
@@ -277,16 +256,14 @@ int main(int argc, const char* argv[])
     auto reference = std::chrono::steady_clock::now();
     std::string output;
 
-    for (size_t i = 0;; ++i)
-    {
+    for (size_t i = 0;; ++i) {
         output.clear();
         output.append(
             "\033[?2026h" // begin synchronized update
-            "\x1b[H" // Cursor Position (CUP)
+            "\x1b[H"      // Cursor Position (CUP)
         );
 
-        for (size_t y = 0; y < dy; ++y)
-        {
+        for (size_t y = 0; y < dy; ++y) {
             const auto idx = (i + y * 2) % num_colors;
             const auto beg = rainbow_indices[idx];
             const auto end = rainbow_indices[idx + dx];
@@ -296,7 +273,7 @@ int main(int argc, const char* argv[])
 
         output.append(
             "\x1b[39;49m" // Foreground/Background color reset (part of SGR)
-            "\x1b[H" // Cursor Position (CUP)
+            "\x1b[H"      // Cursor Position (CUP)
         );
         output.append(std::to_string(kcgs));
         output.append(
@@ -309,8 +286,7 @@ int main(int argc, const char* argv[])
 
         const auto now = std::chrono::steady_clock::now();
         const auto duration = now - reference;
-        if (duration >= std::chrono::seconds(1))
-        {
+        if (duration >= std::chrono::seconds(1)) {
             kcgs = static_cast<size_t>(dx * dy * frame / 1000.0 / std::chrono::duration<double>(duration).count() + 0.5);
             reference = now;
             frame = 0;
